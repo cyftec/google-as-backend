a6b120baad88357354f76a4416b94f44b5018df1
Chandan Kumar <chnkrydv@gmail.com>
2026-09-13 10:12:41 +0530

    refactor: move class definition to separate file

--- packages/oauth/src/index.ts
+++ packages/oauth/src/index.ts
@@ -1,2 +1,3 @@
-export { getOAuthSingleton, type GoogleOAuthConfig, type GoogleOAuth } from "./oauth.ts";
 export { NotAuthenticatedError } from "./errors/not-authenticated-error.ts";
+export type { GoogleOAuthConfig } from "./gis-auth.ts";
+export { getOAuthSingleton, type GoogleOAuth } from "./oauth.ts";
